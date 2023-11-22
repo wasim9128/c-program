49168ec40027eaa45c086ddc6422d3fac2887096
wasim9128 <146606381+wasim9128@users.noreply.github.com>
2023-11-22 15:56:19 +0530

    Update basic_calculation.c
    now this program is updated for the float values ,
    also  added instruction for user to choose which operation user can perform.

--- basic_calculation.c
+++ basic_calculation.c
@@ -1,52 +1,52 @@
 #include <stdio.h>
 #include <conio.h>
 
-int ADD(int a, int b);
-int SUB(int a, int b);
-int MULT(int a, int b);
-int DIV(int a, int b);
+float ADD(float a, float b);
+float SUB(float a, float b);
+float MULT(float a, float b);
+float DIV(float a, float b);
 
-int main()
+float main()
 {
-    int num1, num2, result;
+    float num1, num2, result;
     char choice; // Changed to a single character variable
 
-    printf("Enter Two Numbers To Perform an Operation: \n");
+    prfloatf("Enter Two Numbers To Perform an Operation: \n");
     scanf("%d %d", &num1, &num2);
 
-    printf("Enter the operation you want to perform (+ for addition, - for subtraction): ");
+    prfloatf("Enter the operation you want to perform (+ for addition, - for subtraction, * for Multiplication, / for Div): ");
     scanf(" %c", &choice); // Note the space before %c to consume any newline character.
 
     if (choice == '+')
     {
         result = ADD(num1, num2);
-        printf("The Result Of %d + %d = %d", num1, num2, result);
+        prfloatf("The Result Of %d + %d = %d", num1, num2, result);
     }
     else if (choice == '-')
     {
         result = SUB(num1, num2);
-        printf("The Result Of %d - %d = %d", num1, num2, result);
+        prfloatf("The Result Of %d - %d = %d", num1, num2, result);
     }
     else if (choice == '*')
     {
         result = MULT(num1, num2);
-        printf("The Result Of %d X %d = %d", num1, num2, result);
+        prfloatf("The Result Of %d X %d = %d", num1, num2, result);
     }
     else if (choice == '/')
     {
         if (num1 > num2)
         {
             result = DIV(num1, num2);
-            printf("The Result Of %d / %d = %d", num1, num2, result);
+            prfloatf("The Result Of %d / %d = %d", num1, num2, result);
         }
         else
         {
-            printf("Please! Provide Valid Oprands");
+            prfloatf("Please! Provide Valid Oprands");
         }
     }
     else
     {
-        printf("Invalid operation.");
+        prfloatf("Invalid operation.");
     }
 
     getch();
@@ -54,24 +54,24 @@ int main()
 }
 
 // Definition of ADD() function
-int ADD(int a, int b)
+float ADD(float a, float b)
 {
     return a + b;
 }
 
 // Definition of SUB() function
-int SUB(int a, int b)
+float SUB(float a, float b)
 {
     return a - b;
 }
 // Definition of MULT() function
-int MULT(int a, int b)
+float MULT(float a, float b)
 {
     return a * b;
 }
 
 // Definition of DIV() function
-int DIV(int a, int b)
+float DIV(float a, float b)
 {
     return a / b;
 }
